cff4d5d21047343719d63d788698e4209cec931c
Madmer <51472243+MMadmer@users.noreply.github.com>
2024-03-17 23:06:39 +0700

    Assets clearing

--- Source/SkyFaller/Public/SFCoreTypes.h
+++ Source/SkyFaller/Public/SFCoreTypes.h
@@ -4,9 +4,9 @@
 
 // Health
 
-DECLARE_MULTICAST_DELEGATE(FOnDeathSignature);
-DECLARE_MULTICAST_DELEGATE_TwoParams(FOnHealthChangedSignature, float, float);
-DECLARE_MULTICAST_DELEGATE_OneParam(FOnScoreChangedSignature, int32);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDeathSignature);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnHealthChangedSignature, float, float);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnScoreChangedSignature, int32, Value);
 
 USTRUCT()
 struct FCoreTypes
